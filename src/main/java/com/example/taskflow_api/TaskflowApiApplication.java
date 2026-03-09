ceab066a14efdb9d1b063096a44d2e9c0d8c308c
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-03-09 19:54:26 +0100

    fix: update datasource configuration for WAMP MySQL server

--- src/main/java/com/example/taskflow_api/TaskflowApiApplication.java
+++ src/main/java/com/example/taskflow_api/TaskflowApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskflowApiApplication {
+public class  TaskflowApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskflowApiApplication.class, args);
